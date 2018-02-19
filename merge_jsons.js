c981ce21cb433678a80c752804028ce5c14b2a10
fdnklg <fabian.dinklage@me.com>
2018-02-19 17:25:53 +0100

    chor: json merge

--- merge_jsons.js
+++ merge_jsons.js
@@ -1,13 +1,49 @@
 const fs = require('fs');
 
+let merged = [];
+
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array)
+    }
+};
+
 function readFile(id) {
     return new Promise( (resolve, reject) => {
-        const path = `${id}.json`;
+        const path = `./data/unis/${id}.json`;
         fs.readFile(path, (err,data) => {
             const parsed = JSON.parse(data);
             resolve(parsed);
         })
     })
+};
+
+function fillArray(start, end) {
+    return new Promise( (resolve, reject) => {
+        let arr = [];
+        for (let index = start; index <= end; index++) {
+            arr.push(index);
+        };
+        resolve(arr);
+    });
+};
+
+function writeJson(data, id) {
+    return new Promise( (resolve, object) => {
+        fs.writeFileSync(`./data/${id}.json`, JSON.stringify(data), 'utf8');
+        resolve();
+    })
+};
+
+const mergeFiles = async () => {
+    const arr = await fillArray(1,15);
+
+    const iterate = await asyncForEach( arr, async(item) => {
+        const read = await readFile(item);
+        merged.push(read);
+    });
+
+    const write = await writeJson(merged, 'unis_merged');
 }
 
-const mergeFiles
+mergeFiles();
