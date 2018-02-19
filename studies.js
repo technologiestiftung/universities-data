4d728597c38fbaf8ee2a54a61ddfc5c321d96caf
fdnklg <fabian.dinklage@me.com>
2018-02-19 12:46:01 +0100

    chor: all studies

--- studies.js
+++ studies.js
@@ -53,61 +53,76 @@ function requestNumberStudies(id) {
     })
 }
 
-function requestStudies(data) {
+function requestStudies(id,page) {
  
     return new Promise( (resolve,reject) => {
-        var data_unis = []
-        var id = 9;
-        var index  = 0;
-            
-        var ext = "?tx_szhrksearch_pi1%5Bresults_at_a_time%5D=100";
-        var url = `${urls.studiengaenge}${id}/pn/${index}.html${ext}`;
-
+        
+        const ext = "?tx_szhrksearch_pi1%5Bresults_at_a_time%5D=100";
+        const url = `${urls.studiengaenge}${id}/pn/${page}.html${ext}`;
+        
         req.get(url, (error, response, body) => {
-            $ = cheerio.load(body);
-            
-            var list = {};
-            
-            var num_studiengaenge = $('section.search-results').children('p').text()
-            var num_result = filterCountStudies(num_studiengaenge);
-            list.num_studiengaenge = num_result;
-
-            var study = $('section.search-results').children('div.clearfix').children('section.result-box').each( (i,element) => {
-                var name = $(element).children('h2').text();
-                var detail_link = $(element).children('a').attr('href');
-                var id_studiengang = filterId(detail_link);
-
-                list.name = name;
-                list.id_studiengang = id_studiengang;
-
-                var li = $(element).children('ul').children('li').each( (i, element) => {
-                    var title = $(element).children('span.title').text();
-                    var status = $(element).children('span.status').text();
-                    title !== 'SiT-Passung' ? list[title] = status : null;
-                })
-                data_unis.push(list);
-            });
-
-            resolve(data_unis)
+            if(!error && response.statusCode == 200) { 
+                let data_unis = [];
+                $ = cheerio.load(body);
+                
+                var study = $('section.search-results').children('div.clearfix').children('section.result-box').each( (i,element) => {
+                    var list = {};
+                    var name = $(element).children('h2').text();
+                    var detail_link = $(element).children('a').attr('href');
+                    var id_studiengang = filterId(detail_link);
+                    
+                    list.name = name;
+                    list.id_studiengang = id_studiengang;
+                    
+                    var li = $(element).children('ul').children('li').each( (i, element) => {
+                        var title = $(element).children('span.title').text();
+                        var status = $(element).children('span.status').text();
+                        title !== 'SiT-Passung' ? list[title] = status : null;
+                    })
+                    data_unis.push(list);
+                });
+                resolve(data_unis);
+            }
         })
     })
 }
 
-function queryWrapper() {
-    var data;
-    getData()
-        .then(resolve => {
-            data = resolve;
-            getCookie(urls.uni);
-        }).then( resolve => {
-            requestStudies(200); // echte Id mitgeben
-        }).then( resolve => {
-            console.log(resolve);
-        })
-        
-        // .then( resolve => {
-        //     requestStudies(data);
-        // })
+function writeJson(data, id) {
+    return new Promise( (resolve, object) => {
+        fs.writeFileSync(`./data/${id}.json`, JSON.stringify(data), 'utf8');
+        resolve();
+    });
 };
 
-queryWrapper();
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array)
+    }
+};
+
+function mergeData(json1, json2) {
+    return new Promise( (resolve, reject) => {
+        let arr = [];
+        arr.push(json1);
+        arr.push(json2);
+        resolve(arr);
+    })
+}
+
+const start = async (data) => {
+    await asyncForEach(data, async (item) => {
+        let merged = [];
+        const studyPage1 = await requestStudies(item.id_hochschule,0);
+        const studyPage2 = await requestStudies(item.id_hochschule,1);
+        const merge = await mergeData(studyPage1, studyPage2);
+        const log = await writeJson(merge, item.id_hochschule);
+    });
+}
+
+const asyncQuery = async () => {
+    const cookie = await getCookie(urls.cookie);
+    const data = await getData();
+    start(data);
+};
+
+asyncQuery();
